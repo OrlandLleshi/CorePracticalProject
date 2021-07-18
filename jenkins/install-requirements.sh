1f98edae101e0cac96d4608f55c9994d08af1941
orlandlleshi <orlandlleshi@hotmail.co.uk>
2021-07-18 16:44:48 +0000

    Jenkins fix

--- jenkins/install-requirements.sh
+++ jenkins/install-requirements.sh
@@ -7,7 +7,7 @@ curl https://get.docker.com | sudo bash
 sudo usermod -aG docker jenkins 
 
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/$%7Bversion%7D/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 mkdir -p ~/.local/bin
