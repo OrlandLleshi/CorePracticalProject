58986a56e70ba40d2c753dc5295a79a4d2fe1dfc
OrlandLleshi <orlandlleshi@hotmail.co.uk>
2021-07-18 11:54:45 +0000

    Testing finalised

--- endpoint_api/tests/unit_test.py
+++ endpoint_api/tests/unit_test.py
@@ -12,3 +12,42 @@ class TestResponse(TestBase):
     def test_ricky1(self):
         response = self.client.get(url_for('endpoint'), json={'character':'Ricky', 'background':'Young rich kid that grew up in the city'})
         self.assertEqual(b'Bad Ending', response.data)
+
+    def test_ricky2(self):
+        response = self.client.get(url_for('endpoint'), json={'character':'Ricky', 'background':'Young poor kid that grew up in the city'})
+        self.assertEqual(b'Good Ending', response.data)
+
+    def test_arian1(self):
+        response = self.client.get(url_for('endpoint'), json={'character':'Arian', 'background': 'Young rich kid that grew up in the city'})
+        self.assertEqual(b'Bad Ending', response.data)
+
+    def test_arian2(self):
+        response = self.client.get(url_for('endpoint'), json={'character':'Arian', 'background': 'Young poor kid that grew up in the city'})
+        self.assertEqual(b'Good Ending', response.data)
+
+    def test_thiago1(self):
+        response= self.client.get(url_for('endpoint'), json={'character':'Thiago', 'background': 'Young rich kid that grew up in the city'})
+        self.assertEqual(b'Good Ending', response.data)
+
+    def test_thiago2(self):
+        response= self.client.get(url_for('endpoint'), json={'character':'Thiago', 'background': 'Young poor kid that grew up in the city'})
+        self.assertEqual(b'Bad Ending', response.data)
+
+    def test_carson1(self):
+        response=self.client.get(url_for('endpoint'), json={'character':'Carson', 'background': 'Young rich kid that grew up in the city'})
+        self.assertEqual(b'Good Ending', response.data)
+
+    def test_carson2(self):
+        response=self.client.get(url_for('endpoint'), json={'character':'Carson', 'background': 'Young poor kid that grew up in the city'})
+        self.assertEqual(b'Good Ending', response.data)
+
+    def test_duncan1(self):
+        response= self.client.get(url_for('endpoint'), json={'character':'Duncan', 'background':'Young rich kid that grew up in the city'})
+        self.assertEqual(b'Bad Ending', response.data)
+
+    def test_duncan2(self):
+        response= self.client.get(url_for('endpoint'), json={'character':'Duncan', 'background':'Young poor kid that grew up in the city'})
+        self.assertEqual(b'Good Ending', response.data)
+
+    
+
