dd467af35b5dc49780a40f1eaf32abbd8ea0790a
orlandlleshi <orlandlleshi@hotmail.co.uk>
2021-07-18 17:22:33 +0000

    Jenkins fix

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,7 +2,7 @@
 
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml Orland@flask:/home/docker-compose.yaml 
 
-ssh -i ~/.ssh/ansible Orland@flask << EOF
+ssh -i ~/.ssh/ansible_id_rsa Orland@flask << EOF
     export DATABASE_URI=${DATABASE_URI}
     docker stack deploy --compose-file docker-compose.yaml project 
 
